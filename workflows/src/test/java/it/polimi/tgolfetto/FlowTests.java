967caa7178f184f022e49a28f2467e415dd9578b
Golfetto Thomas <golfetto.thomas@gmail.com>
2023-03-14 18:09:04 +0100

    unit testing

--- workflows/src/test/java/it/polimi/tgolfetto/FlowTests.java
+++ workflows/src/test/java/it/polimi/tgolfetto/FlowTests.java
@@ -1,62 +1,143 @@
 package it.polimi.tgolfetto;
 
 import com.google.common.collect.ImmutableList;
+import it.polimi.tgolfetto.flows.membershipFlows.*;
 import net.corda.bn.states.MembershipState;
 import net.corda.core.contracts.StateAndRef;
+import net.corda.core.contracts.UniqueIdentifier;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.node.services.Vault;
 import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.testing.core.TestIdentity;
 import net.corda.testing.node.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class FlowTests {
     private MockNetwork network;
-    private StartedMockNode a;
-    private StartedMockNode b;
+    private StartedMockNode networkOperator;
+    private StartedMockNode textileFirm;
+    private StartedMockNode certifier;
 
     @Before
     public void setup() {
-       /* network = new MockNetwork(new MockNetworkParameters().withCordappsForAllNodes(ImmutableList.of(
-                TestCordapp.findCordapp("net.corda.samples.businessmembership.contracts"),
-                TestCordapp.findCordapp("net.corda.samples.businessmembership.flows"),
-                TestCordapp.findCordapp("net.corda.bn.flows"),
-                TestCordapp.findCordapp("net.corda.bn.states")))
-                .withNotarySpecs(ImmutableList.of(new MockNetworkNotarySpec(CordaX500Name.parse("O=Notary,L=London,C=GB"))))
+        network = new MockNetwork(new MockNetworkParameters().withThreadPerNode(false).withCordappsForAllNodes(ImmutableList.of(
+                        TestCordapp.findCordapp("it.polimi.tgolfetto.contracts"),
+                        TestCordapp.findCordapp("it.polimi.tgolfetto.flows"),
+                        TestCordapp.findCordapp("net.corda.bn.flows"),
+                        TestCordapp.findCordapp("net.corda.bn.states")))
+                .withNotarySpecs(ImmutableList.of(new MockNetworkNotarySpec(CordaX500Name.parse("O=Notary,L=Rome,C=IT"))))
         );
-        a = network.createPartyNode(null);
-        b = network.createPartyNode(null);
-        network.runNetwork();*/
+        networkOperator = network.createPartyNode(CordaX500Name.parse("O=NetworkOperator,L=Milan,C=IT"));
+        textileFirm = network.createPartyNode(CordaX500Name.parse("O=TextileManufacturer1,L=Prato,C=IT"));
+        certifier = network.createPartyNode(CordaX500Name.parse("O=Certifier,L=Zurich,C=CH"));
+        network.runNetwork();
     }
 
     @After
     public void tearDown() {
-       // network.stopNodes();
+        network.stopNodes();
     }
 
     @Test
     public void createNetworkTest() throws ExecutionException, InterruptedException {
-      /*  CreateNetwork flow = new CreateNetwork();
-        Future<String> future = a.startFlow(flow);
+        CreateNetwork flow = new CreateNetwork();
+        Future<String> future = networkOperator.startFlow(flow);
         network.runNetwork();
         String resString = future.get();
-        System.out.println(resString);
 
         int subString = resString.indexOf("NetworkID: ");
-        String networkId = resString.substring(subString+11);
-        System.out.println("-"+ networkId+"-");
+        String networkId = resString.substring(subString + 11);
 
-        QueryCriteria inputCriteria = new QueryCriteria.LinearStateQueryCriteria().withStatus(Vault.StateStatus.UNCONSUMED);
-        MembershipState storedMembershipState = a.getServices().getVaultService()
-                .queryBy(MembershipState.class,inputCriteria).getStates().get(0).getState().getData();
-        System.out.println(storedMembershipState.getNetworkId());
-
+        MembershipState storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(0).getState().getData();
+        System.out.println("### createNetworkTest: " + networkId);
         assert (storedMembershipState.getNetworkId().equals(networkId));
-*/
+
+    }
+
+    @Test
+    public void requestMembershipTest() {
+        CreateNetwork flow = new CreateNetwork();
+        Future<String> future = networkOperator.startFlow(flow);
+        network.runNetwork();
+        MembershipState storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(0).getState().getData();
+        String networkId = storedMembershipState.getNetworkId();
+        RequestMembership requestMembershipFlow = new RequestMembership(networkOperator.getInfo().getLegalIdentities().get(0), networkId);
+        textileFirm.startFlow(requestMembershipFlow);
+        network.runNetwork();
+        requestMembershipFlow = new RequestMembership(networkOperator.getInfo().getLegalIdentities().get(0), networkId);
+        certifier.startFlow(requestMembershipFlow);
+        network.runNetwork();
+        List<StateAndRef<MembershipState>> storedMembershipStates = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates();
+        System.out.println("### requestMembershipTest: " + storedMembershipStates);
+        System.out.println("### requestMembershipTest: " + storedMembershipStates.size());
+        assert (storedMembershipStates.size() == 3);
+    }
+
+    @Test
+    public void queryAllMembersTest() throws ExecutionException, InterruptedException {
+        CreateNetwork flow = new CreateNetwork();
+        networkOperator.startFlow(flow);
+        network.runNetwork();
+        QueryAllMembers queryFlow = new QueryAllMembers();
+        Future<String> future = networkOperator.startFlow(queryFlow);
+        network.runNetwork();
+        String resString = future.get();
+        MembershipState storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(0).getState().getData();
+        assert(resString.contains(storedMembershipState.getLinearId().toString()) && resString.contains(storedMembershipState.getStatus().toString()));
+    }
+
+    @Test
+    public void activateMemberTest() throws ExecutionException, InterruptedException {
+        CreateNetwork flow = new CreateNetwork();
+        networkOperator.startFlow(flow);
+        network.runNetwork();
+        MembershipState storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(0).getState().getData();
+        String networkId = storedMembershipState.getNetworkId();
+        RequestMembership requestMembershipFlow = new RequestMembership(networkOperator.getInfo().getLegalIdentities().get(0), networkId);
+        textileFirm.startFlow(requestMembershipFlow);
+        network.runNetwork();
+        storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(1).getState().getData();
+        ActivateMember activateMemberFlow = new ActivateMember(storedMembershipState.getLinearId());
+        Future<String> future= networkOperator.startFlow(activateMemberFlow);
+        network.runNetwork();
+        System.out.println(future.get());
+        storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(1).getState().getData();
+        System.out.println("### activateMemberTest: " + storedMembershipState);
+        assert(storedMembershipState.getStatus().toString().equals("ACTIVE"));
+    }
+
+
+    @Test
+    public void createNetworkSubGroupTest(){
+        /*
+        CreateNetwork flow = new CreateNetwork();
+        networkOperator.startFlow(flow);
+        network.runNetwork();
+        MembershipState storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(0).getState().getData();
+        String networkId = storedMembershipState.getNetworkId();
+        RequestMembership requestMembershipFlow = new RequestMembership(networkOperator.getInfo().getLegalIdentities().get(0), networkId);
+        textileFirm.startFlow(requestMembershipFlow);
+        network.runNetwork();
+        storedMembershipState = networkOperator.getServices().getVaultService()
+                .queryBy(MembershipState.class).getStates().get(1).getState().getData();
+        ActivateMember activateMemberFlow = new ActivateMember(new UniqueIdentifier(storedMembershipState.getNetworkId()));
+        networkOperator.startFlow(activateMemberFlow);
+        network.runNetwork();
+         */
     }
 }
